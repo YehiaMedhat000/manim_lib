3fd4f33a3c6b0ee56b73d6fe2eed5ee823e95951
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-08-25 16:00:21 +0300

    Built most of the SR_Numline scene * Still failing at moving, showing, removing mobjects when I want or   according to specific conditions * I want to make the last animation to multiply the value by 1/2 in the   form of fraction, but because ValueTracker expects int only so it   becomes 0 * I want to make the expression of multiplication squish or supress into   one character each time it occurs.

--- 1p1t_1lesson.py
+++ 1p1t_1lesson.py
@@ -536,16 +536,47 @@ class SR_Numline(Scene):
         
         # Initialize Mobjects
         num_l_0 = NumberLine(x_range=[-10,10],include_numbers=True,include_ticks=True,length=13,color=BLUE)
-        arr = Arrow(start=num_l_0.n2p(0)+(UP*0.5),end=num_l_0.n2p(3)+(UP*0.5),color=YELLOW)
-        num = always_redraw(lambda: DecimalNumber(number=(arr.end[0]-arr.start[0]),
-                                                    num_decimal_places=0,include_sign=True).next_to(arr,UP))
+        num = ValueTracker(3) 
+        multiplier = ValueTracker(-2)
+        p_end = ValueTracker(3)
 
+        arr_0 = always_redraw(lambda: Arrow(start=num_l_0.n2p(0)+(UP*0.5),
+                                            end=num_l_0.n2p(p_end.get_value())+(UP*0.5),color=BLUE,buff=0))
 
+        num_0 = always_redraw(lambda: DecimalNumber(number=num.get_value(),num_decimal_places=0,
+                                                    include_sign=True,color=arr_0.get_color()))
+
+        f_0 = always_redraw(lambda: MathTex('\\times').next_to(num_0))
+
+        num_1 = always_redraw(lambda: DecimalNumber(number=multiplier.get_value(),num_decimal_places=0,include_sign=True))
+
+        arr_0.add_updater(lambda mob: mob.set_color(RED) if p_end.get_value() < 0 else mob.set_color(BLUE))
+        num_0.add_updater(lambda mob: mob.next_to(arr_0,UP))
+        f_0.add_updater(lambda mob: mob.next_to(num_0))
+        num_1.add_updater(lambda mob: mob.next_to(f_0))
+        
         # Playing animations
-        self.play(Write(num_l_0,rate_func=rate_functions.ease_in_expo),
-                  Create(arr),Write(num))
-        self.wait()
-        self.play(arr.animate.set(start=num_l_0.n2p(0)+(UP*0.5),end=num_l_0.n2p(-6)+(UP*0.5)))
+        self.play(Write(num_l_0))
+        self.wait(2)
+        self.play(Create(arr_0),Write(num_0))
+        self.wait(2)
+        self.play(Write(f_0),Write(num_1))
+        self.wait(2)
+        self.play(p_end.animate.set_value(-6),
+                  num.animate.set_value(-6),
+                  multiplier.animate.set_value(1))
+        self.wait(2)
+        self.play(multiplier.animate.set_value(-1))
+        self.wait(2)
+        self.play(p_end.animate.set_value(6),
+                  num.animate.set_value(6),
+                  multiplier.animate.set_value(1))
+        self.wait(2)
+        self.play(multiplier.animate.set_value(2),
+                  FadeOut(f_0))
+        self.wait(2)
+        self.play(p_end.animate.set_value(3),
+                  num.animate.set_value(3),
+                  multiplier.animate.set_value(1),
+                  FadeOut())
         self.wait(3)
-        
-        
